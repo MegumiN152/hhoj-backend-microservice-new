adbc216c117d79383f05f86e51d2dcd1376867ee
hh <3105755134@qq.com>
2025-04-03 10:41:47 +0800

    启用异步任务注解

--- hhoj-backend-question-service/src/main/java/com/hh/hhojbackendquestionservice/HhBackendQuestionServiceApplication.java
+++ hhoj-backend-question-service/src/main/java/com/hh/hhojbackendquestionservice/HhBackendQuestionServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -17,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.hh.hhojbackendserviceclient.service")
+@EnableAsync
 public class HhBackendQuestionServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(HhBackendQuestionServiceApplication.class, args);
